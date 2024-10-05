81fe74753fdbe9ab2551a1ba75524b6fecc05e5e
JackS0l0 <you@example.com>
2024-10-05 12:03:48 +0400

    modified:   main/__pycache__/views.cpython-312.pyc 	modified:   main/views.py 	modified:   static/pdfjs/web/viewer.html

--- main/views.py
+++ main/views.py
@@ -3,14 +3,14 @@ from .models import Books, İnfo
 from django.views.generic import DetailView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-# @login_required
+@login_required
 def home(request):
     data={
         'title': 'Biologiya - Quliyeva Suqra',
         'books': Books.objects.all(),
     }
     return render(request,'home.html',data)
-# @method_decorator(login_required, name='dispatch')  # Apply login_required to the class-based view
+@method_decorator(login_required, name='dispatch')  # Apply login_required to the class-based view
 class DetailBook(DetailView):
     model = Books
     context_object_name = 'books'
